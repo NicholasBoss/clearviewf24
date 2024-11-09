8b2eb8e357abddcccbed8df72986045f832e616a
camdenjstoker <sto22016@byui.edu>
2024-11-09 10:54:39 -0700

    Create inserts_stoker.sql

---
+++ database/spellman_l/inserts_stoker.sql
@@ -0,0 +1,66 @@
+INSERT INTO phantom_color ()
+VALUES();
+
+INSERT INTO phantom_build_out()
+VALUES();
+
+INSERT INTO meshlock()
+VALUES();
+
+INSERT INTO phantom_mesh()
+VALUES();
+
+INSERT INTO meshlock_mesh()
+VALUES();
+
+INSERT INTO type_of_screen()
+VALUES();
+
+INSERT INTO screen_color()
+VALUES();
+
+INSERT INTO wood_type()
+VALUES();
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO est_placement(placement)
+VALUES('Outside', 'Inside', 'Under Facing In', 'Under Facing Out');
+
+INSERT INTO est_housing_series(housing series)
+VALUES('3 Round', '4 Round', '5 Round', '4 Square', '5 Square');
+
+INSERT INTO est_drive_side(drive)
+VALUES('Left', 'Right');
+
+INSERT INTO est_hembar(hembar)
+VALUES('Short', 'Tall', 'Tall Reinforced');
+
+INSERT INTO est_pilebrush(Pile Brush)
+VALUES('Single 1/2" Black', 'Single 1" Black', 'Single 2" Black', 'Single 3/4 White', 'Double 1/2" Black', 'Double 1" Black', 'Double 2" Black', 'Double 3/4" White');
+
+INSERT INTO est_brush_location(Brush Location)
+VALUES('Side', 'Bottom');
+
+INSERT INTO est_cord_length(Cord Length)
+Values('12" (Std)', '24 (Upgrade)')
+
+INSERT INTO est_mount_type(Tracks)
+VALUES('Surface Mount', 'Recessed (Side)');
+
+INSERT INTO est_top_opening_width()
+VALUES();
+
+INSERT INTO est_left_buildout()
+VALUES();
+
+INSERT INTO est_right_buildout()
+VALUES();
+
+INSERT INTO est_add_buildout()
+VALUES();
+
+INSERT INTO est_left_track()
+VALUES();
+
+INSERT INTO est_right_track()
+VALUES();
