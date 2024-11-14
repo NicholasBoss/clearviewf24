d1bd3340ca2e314c02627d9bf54ecfa9a8bf5e16
bartojer <bartojer@gmail.com>
2024-11-13 19:28:59 -0700

    started adding createOrder to ordersController

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -32,4 +32,21 @@ ordersController.buildDelete = async function(req, res){
     })
 }
 
+ordersController.createOrder = async function(req, res){
+    const { product_name, measurement_name, size_type, fastener_type, color_name, mesh_type, mirage_build_out, mirage_3500_handle } = req.body
+    console.log('Creating order')
+    try {
+        const order = await ordersModel.createOrder(
+            product_name,
+            measurement_name,
+            size_type,
+            fastener_type,
+            color_name,
+            mesh_type,
+            mirage_build_out,
+            mirage_3500_handle
+        )
+    }
+}
+
 module.exports = ordersController
