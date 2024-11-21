d1e7a1946906bd88078db90a20e200bcaf0c982b
Kyle Murdock <166570055+kylemur@users.noreply.github.com>
2024-11-21 12:31:25 -0700

    setup

--- setup.bat
+++ setup.bat
@@ -33,24 +33,20 @@ nvm ls
 REM Download and move .env file
 echo Please download the .env file from Discord and move it into the clearviewf24 folder.
 pause
-echo Renaming the file to .env...
-rename "clearviewf24\*.env" ".env"
+
 
 REM Enable pnpm
+echo Enabling PNPM
 corepack enable pnpm
-if %errorlevel% neq 0 (
-    npm install -g pnpm
-)
+
 
 REM Prepare pnpm
 corepack prepare pnpm@latest --activate
 
 REM Install dependencies
+echo Installing Dependencies...
 pnpm add bcryptjs body-parser connect-flash connect-pg-simple cookie-parser dotenv ejs express express-ejs-layouts express-messages express-session express-validator jsonwebtoken nodemon pg
-if %errorlevel% neq 0 (
-    echo Failed to install dependencies. Please try again.
-    exit /b 1
-)
+
 
 REM Run the development server
 pnpm run dev
